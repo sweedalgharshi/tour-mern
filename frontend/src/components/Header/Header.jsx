b5b56e6ada40c3455a2241ab859767d87dda69ac
sweedalgharshi <sweedalgracius@gmail.com>
2023-07-24 18:24:17 +0530

    search bar, subtitle components

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,72 @@
+import { Container, Row, Button } from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
+
+import logo from "../../assets/images/logo.png";
+import "./header.css";
+
+const nav__links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex align-items-center justify-content-between">
+            {/* =========== Logo =========== */}
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+
+            {/* =========== Logo END =========== */}
+
+            {/* =========== Menu Start =========== */}
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {nav__links.map((link, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={link.path}
+                      className={(navCLass) => (navCLass.isActive ? "active__link" : "")}
+                    >
+                      {link.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* =========== Menu END =========== */}
+
+            <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nav__btn d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+
+              <span className="mobile__menu">
+                <i className="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
 }
 export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,58 @@
+import "../styles/home.css";
+
+import { Container, Row, Col } from "reactstrap";
+
+import worldImg from "../assets/images/world.png";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import Subtitle from "../shared/Subtitle";
+import SearchBar from "../shared/SearchBar";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating <span className="highlight"> memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid repellendus
+                  accusantium aliquam consequuntur iure, veniam aspernatur excepturi rerum assumenda
+                  optio?
+                </p>
+              </div>
+            </Col>
+
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 }
 export default Home;
