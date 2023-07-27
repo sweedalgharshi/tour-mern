1c63420eda1eb64ec0702faa2cb5a83bddec5b7a
sweedalgharshi <sweedalgracius@gmail.com>
2023-07-27 12:59:46 +0530

    initial commit with mongodb connection

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// database connection
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Mongo DB database connected");
+  } catch (err) {
+    console.log("MongoDb database connection failed", port);
+  }
+};
+
+// for testing
+// app.get("/", (req, res) => {
+//   res.send("app is working");
+// });
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.listen(port, () => {
+  connect();
+  console.log(`server listening on port ${port}`);
+});
