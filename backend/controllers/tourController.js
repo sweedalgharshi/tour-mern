16644c29503495d267316daa7928ad11c5520254
sweedalgharshi <sweedalgracius@gmail.com>
2023-07-31 16:49:52 +0530

    added reviews controller, routes

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -69,7 +69,7 @@ export const deleteTour = async (req, res) => {
 export const getSingleTour = async (req, res) => {
   const id = req.params.id;
   try {
-    const tour = await Tour.findById(id);
+    const tour = await Tour.findById(id).populate("reviews");
 
     res.status(200).json({
       success: true,
@@ -92,6 +92,7 @@ export const getAllTour = async (req, res) => {
   console.log(page);
   try {
     const tours = await Tour.find({})
+      .populate("reviews")
       .skip(page * 8)
       .limit(8);
 
@@ -123,7 +124,7 @@ export const getTourBySearch = async (req, res) => {
       city,
       distance: { $gte: distance },
       maxGroupSize: { $gte: maxGroupSize },
-    });
+    }).populate("reviews");
 
     res.status(200).json({
       success: true,
@@ -142,7 +143,7 @@ export const getTourBySearch = async (req, res) => {
 
 export const getFeaturedTours = async (req, res) => {
   try {
-    const tours = await Tour.find({ featured: true }).limit(8);
+    const tours = await Tour.find({ featured: true }).populate("reviews").limit(8);
 
     res.status(200).json({
       success: true,
