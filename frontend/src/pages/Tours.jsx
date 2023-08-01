b20058aefec0d6ad372e97bd29429eb0362e1707
sweedalgharshi <sweedalgracius@gmail.com>
2023-08-01 17:16:02 +0530

    made tours component dyanamic integrated with backend

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -3,21 +3,28 @@ import { useState, useEffect } from "react";
 import CommonSection from "../shared/CommonSection";
 import "../styles/tour.css";
 
-import tourData from "../assets/data/tours";
+// import tourData from "../assets/data/tours";
 
 import Newsletter from "../shared/Newsletter";
 import TourCard from "../shared/TourCard";
 import SearchBar from "../shared/SearchBar";
 import { Container, Row, Col } from "reactstrap";
 
+import useFetch from "../hooks/useFetch";
+import { BASE_URL } from "../utils/config";
+
 function Tours() {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
 
+  const { data: tours, error, loading } = useFetch(`${BASE_URL}/tours?page=${page}`);
+  const { data: tourCount } = useFetch(`${BASE_URL}/tours/search/getTourCount`);
+
   useEffect(() => {
-    const pages = Math.ceil(5 / 4); // later will use backend data count
+    const pages = Math.ceil(tourCount / 8); // later will use backend data count
     setPageCount(pages);
-  }, [page]);
+    window.scrollTo(0, 0);
+  }, [page, tourCount, tours]);
 
   return (
     <>
@@ -31,27 +38,31 @@ function Tours() {
       </section>
       <section className="pt-0">
         <Container>
-          <Row>
-            {tourData?.map((tour) => (
-              <Col key={tour.id} lg="3">
-                <TourCard tour={tour} />
-              </Col>
-            ))}
+          {loading && <h4 className="text-center pt-5">Loading.....</h4>}
+          {error && <h4 className="text-center pt-5">{error}</h4>}
+          {!loading && !error && (
+            <Row>
+              {tours?.map((tour) => (
+                <Col key={tour._id} lg="3">
+                  <TourCard tour={tour} />
+                </Col>
+              ))}
 
-            <Col lg="12">
-              <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-                {[...Array(pageCount).keys()].map((number) => (
-                  <span
-                    key={number}
-                    onClick={() => setPage(number)}
-                    className={page === number ? "active__page" : ""}
-                  >
-                    {number + 1}
-                  </span>
-                ))}
-              </div>
-            </Col>
-          </Row>
+              <Col lg="12">
+                <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                  {[...Array(pageCount).keys()].map((number) => (
+                    <span
+                      key={number}
+                      onClick={() => setPage(number)}
+                      className={page === number ? "active__page" : ""}
+                    >
+                      {number + 1}
+                    </span>
+                  ))}
+                </div>
+              </Col>
+            </Row>
+          )}
         </Container>
       </section>
       <Newsletter />
