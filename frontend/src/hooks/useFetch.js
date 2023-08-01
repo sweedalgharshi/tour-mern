324f0044d8456cf1a9121cf429b586efee7cb9a1
sweedalgharshi <sweedalgracius@gmail.com>
2023-08-01 13:26:06 +0530

    featured tour list integrated with backend

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 
-const useFetch = () => {
+const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const fetchData = async (url) => {
+    const fetchData = async () => {
       setLoading(true);
 
       try {
@@ -20,13 +20,14 @@ const useFetch = () => {
         const result = await res.json();
         console.log(result);
         setData(result.data);
+        setLoading(false);
       } catch (err) {
         setError(err.message);
         setLoading(false);
       }
     };
     fetchData();
-  }, []);
+  }, [url]);
   return { data, error, loading };
 };
 
